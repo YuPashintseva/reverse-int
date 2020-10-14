da24409477489b24340e6ebb37f5e32bbaa2a82b
YuPashintseva <juliyapashintseva@gmail.com>
2020-10-14 10:20:20 +0600

    feat: solve the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+
+  let reversed = n.toString().split('').reverse();//.join('');
+  if (reversed[reversed.length-1] === "-"){
+    reversed.pop();
+  }
+  return reversed.join('');
 }
